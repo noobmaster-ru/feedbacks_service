3d0d1efb6c2a45b02c8f95c8fb0e9f5ea5f2205f
KirillKozlov <kirillandreevickozlov@yandex.ru>
2025-08-02 16:38:02 +0300

    d

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ import json
 import os 
 import shutil
 
-from parse_module.parse_wb_site_class import ParseWbSiteClass
+from parse_module.parse_feedbacks import ParseFeedbacks
 
 from constants import ARTICLES_WB
 
 async def main():
-    SEMAPHORE = asyncio.Semaphore(100)  # можно поставить 20 или 50
-    parser = ParseWbSiteClass(SEMAPHORE)
     feedbacks = {}
     tasks = []
     async with aiohttp.ClientSession() as session:
         for i in range(len(ARTICLES_WB)):
-            tasks.append(parser.parse_last_five_feedbacks_rating_nm_id(session, ARTICLES_WB[i], feedbacks))
+            tasks.append(ParseFeedbacks.parse_last_five_feedbacks_rating_nm_id(session, ARTICLES_WB[i], feedbacks))
         await asyncio.gather(*tasks)
 
         with open(
